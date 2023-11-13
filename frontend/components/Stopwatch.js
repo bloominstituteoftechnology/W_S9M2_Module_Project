04a3775cf1f853770bf32fa11c7042cd034da6df
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-11-13 20:35:40 +0100

    wip

--- frontend/components/Stopwatch.js
+++ frontend/components/Stopwatch.js
@@ -6,14 +6,14 @@ export default function Interval() {
   return (
     <div className="widget">
       <p className="info">Click Split to update total running time</p>
-      <p>Total running time is <span>14 seconds</span></p>
+      <p>Total running time is <span>{15} seconds</span></p>
       <p>
         <button>Split</button>
         <button>Lap</button>
       </p>
       <ul>
         <li>Lap {1}: <span>{7} sec</span></li>
-        <li>Lap {2}: <span>{13} sec</span></li>
+        <li>Lap {2}: <span>{8} sec</span></li>
       </ul>
     </div>
   )
